45df5a935921af461aacdd73b74e38b6f8bd1302
Magne Haneberg <magnehh@stud.ntnu.no>
2015-11-08 18:50:17 +0100

    Finished subsect on s matrix.

--- code/reflect_coeff.m
+++ code/reflect_coeff.m
@@ -1,7 +1,7 @@
 function [ reflect_coeff ] = reflect_coeff( z1,z2 )
 %REFLECT_COEFF This function calculates the reflection coefficient
 
-relect_coeff = (z2 - z1)/(z2 + z1);
+reflect_coeff = (z2 - z1)/(z2 + z1);
 
 end
 
